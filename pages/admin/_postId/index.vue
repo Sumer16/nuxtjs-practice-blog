cb06234971d06c97d4689776dae5eaaf8d7b7677
Sumer Patel <sumerdpatel@gmail.com>
2020-08-10 12:35:26 +0530

    added admin code

---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="admin-post-page">
+    <section class="update-form">
+      <AdminPostForm :post="loadedPost" />
+    </section>
+  </div>
+</template>
+
+<script>
+import AdminPostForm from '@/components/Admin/AdminPostForm'
+
+export default {
+  components: {
+    AdminPostForm
+  },
+  data() {
+    return {
+      loadedPost: {
+        author: 'Sumer Patel',
+        title: 'Mah Life',
+        content: 'Jhaandu baam, ek kaam teen baam',
+        thumbnailLink: 'https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
