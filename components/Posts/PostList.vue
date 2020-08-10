1b8427cc964ac35a94fe1bd22b495bba200ee4ea
Sumer Patel <sumerdpatel@gmail.com>
2020-08-10 09:16:55 +0530

    added navigation

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="post-list">
+      <PostPreview 
+      id="1" 
+      thumbnail="https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" 
+      title="Hello there!" 
+      previewText="Falana Dikana" />
+      <PostPreview 
+      id="2" 
+      thumbnail="https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" 
+      title="Hello there again!" 
+      previewText="Falana Dikana" />
+      <PostPreview 
+      id="3" 
+      thumbnail="https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" 
+      title="Hello there again now!" 
+      previewText="Falana Dikana"/>
+    </section>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+
+export default {
+  components: {
+    PostPreview
+  }
+}
+</script>
+
+<style scoped>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+}
+</style>>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,33 +3,17 @@
     <section class="intro">
       <h1>Get the latest tech news</h1>
     </section>
-    <section class="featured-posts">
-      <nuxt-link v-bind:to="'/posts/' + 1" class="post-preview">
-        <article>
-          <div class="post-thumbnail" style="background-image: url('https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')">
-          </div>
-          <div class="post-content">
-            <h1>Post Title 1</h1>
-            <h1>Falana Dikana</h1>
-          </div>
-        </article>
-      </nuxt-link>
-      <nuxt-link v-bind:to="'/posts/' + 2" class="post-preview">
-        <article>
-          <div class="post-thumbnail" style="background-image: url('https://images.pexels.com/photos/193349/pexels-photo-193349.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')">
-          </div>
-          <div class="post-content">
-            <h1>Post Title 2</h1>
-            <h1>Dikana Falana</h1>
-          </div>
-        </article>
-      </nuxt-link>
-    </section>
+    <PostList />
   </div>
 </template>
 
 <script>
-export default {}
+import PostList from '@/components/Posts/PostList'
+export default {
+  components: {
+    PostList
+  }
+}
 </script>
 
 <style>
@@ -40,6 +24,7 @@ export default {}
   box-sizing: border-box;
   background-position: center;
   background-size: cover;
+  background-image: url('~assets/images/img2.jpg');
   text-align: center;
 }
 
@@ -64,49 +49,5 @@ export default {}
   }
 }
 
-.featured-posts {
-  display: flex;
-  padding: 20px;
-  box-sizing: border-box;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
-  gap: 1rem;
-}
 
-.post-preview {
-  border: 1px solid #ccc;
-  box-shadow: 0 2px 2px #ccc;
-  background-color: white;
-  width: 90%;
-}
-
-a {
-  text-decoration: none;
-  color: black;
-}
-
-@media (min-width: 850px) {
-  .post-preview {
-    width: 400px;
-    margin: 10px;
-  }
-}
-
-.post-thumbnail {
-  width: 100%;
-  height: 200px;
-  background-position: center;
-  background-size: cover;
-}
-
-.post-content {
-  padding: 10px;
-  text-align: center;
-}
-
-a:hover .post-content,
-a:active .post-content {
-  background-color: #ccc;
-}
 </style>
