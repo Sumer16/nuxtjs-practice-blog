efb1b1127a8c8d5b2f0c7974a5d2412cce97cb6c
Sumer Patel <sumerdpatel@gmail.com>
2020-08-11 18:36:02 +0530

    learnt to play around

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,27 +15,41 @@ export default {
   ** See https://nuxtjs.org/api/configuration-head
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'WD Blog',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'My cool web development blog' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Open+Sans:ital@1&display=swap' }
     ]
   },
+
+  loading: { 
+    color: '#0df976', 
+    height: '5px', 
+    duration: 5000 
+  },
+
+  loadingIndicator: {
+    name: 'circle',
+    color: '#0df976'
+  },
   /*
   ** Global CSS
   */
   css: [
+    '~assets/styles/main.css'
   ],
   /*
   ** Plugins to load before mounting the App
   ** https://nuxtjs.org/guide/plugins
   */
   plugins: [
+    '~plugins/core-components.js',
+    '~plugins/date-filter.js'
   ],
   /*
   ** Auto import components
@@ -51,11 +65,23 @@ export default {
   ** Nuxt.js modules
   */
   modules: [
+    '@nuxtjs/axios',
   ],
+  axios: {
+    baseURL: process.env.BASE_URL || '{YOUR_DATABASE_URL}',
+    credentials: false
+  },
   /*
   ** Build configuration
   ** See https://nuxtjs.org/api/configuration-build/
   */
   build: {
+  },
+  env: {
+    baseUrl: process.env.BASE_URL || '{YOUR_DATABASE_URL}'
+  },
+  transition: {
+    name: 'fade',
+    mode: 'out-in'
   }
 }
