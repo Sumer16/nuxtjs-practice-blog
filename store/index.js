9818957947103c1d0acf396850cb3dfbf193ccf8
Sumer Patel <sumerdpatel@gmail.com>
2020-08-13 01:31:02 +0530

    modifications to store.js

--- store/index.js
+++ store/index.js
@@ -1,9 +1,11 @@
 import Vuex from 'vuex';
+import Cookie from 'js-cookie'
 
 const createStore = () => {
   return new Vuex.Store({
     state: {
-      loadedPosts: []
+      loadedPosts: [],
+      token: null
     },
     mutations: {
       setPosts(state, posts) {
@@ -15,6 +17,12 @@ const createStore = () => {
       editPost(state, editedPost) {
         const postIndex = state.loadedPosts.findIndex(post => post.id === editedPost.id);
         state.loadedPosts[postIndex] = editedPost
+      },
+      setToken(state, token) {
+        state.token = token;
+      },
+      clearToken(state) {
+        state.token = null;
       }
     },
     actions: {
@@ -39,7 +47,7 @@ const createStore = () => {
           ...post, 
           updatedDate: new Date()
         }
-        return this.$axios.$post('/posts.json', createdPost)
+        return this.$axios.$post('/posts.json?auth=' + vuexContext.state.token, createdPost)
         .then(data => {
           vuexContext.commit('addPost', { ...createdPost, id: data.name });
         })
@@ -48,16 +56,76 @@ const createStore = () => {
         });
       },
       editPost(vuexContext, editedPost) {
-        return this.$axios.$put('/posts/' + editedPost.id + '.json', editedPost)
+        return this.$axios.$put('/posts/' + editedPost.id + '.json?auth=' + vuexContext.state.token , editedPost)
         .then(data => {
           vuexContext.commit('editPost', editedPost);
         })
         .catch(e => console.log(e))
+      },
+      authUser(vuexContext, authData) {
+        let authUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + process.env.fbAPIKey;
+        if (!authData.isLogin) {
+          authUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + process.env.fbAPIKey;
+        }
+        return this.$axios.$post(authUrl, {
+          email: authData.email,
+          password: authData.password,
+          returnSecureToken: true
+        }).then(result => {
+          console.log(result);
+          vuexContext.commit('setToken', result.idToken);
+          localStorage.setItem('token', result.idToken)
+          localStorage.setItem('tokenExpiration', new Date().getTime() + +result.expiresIn * 1000);
+
+          Cookie.set('jwt', result.idToken);
+          Cookie.set('expirationDate', new Date().getTime() + +result.expiresIn * 1000);
+          return this.$axios.$post('http://localhost:3000/api/track-data', {
+            data: 'Authenticated!'
+          })
+        }).catch(error => {
+          console.log(error);
+        });
+      },
+      initAuth(vuexContext, req) {
+        let token;
+        let expirationDate;
+        if(req) {
+          if(!req.headers.cookie) {
+            return;
+          }
+          const jwtCookie = req.headers.cookie.split(';').find(c => c.trim().startsWith('jwt='));
+          if(!jwtCookie) {
+            return;
+          }
+          token = jwtCookie.split('=')[1];
+          expirationDate = req.headers.cookie.split(';').find(c => c.trim().startsWith('expirationDate=')).split('=')[1];
+        } else if(process.client) {
+          token = localStorage.getItem('token');
+          expirationDate = localStorage.getItem('tokenExpiration');
+        }
+        if(new Date().getTime() > expirationDate || !token) {
+          console.log('No or Invalid Token');
+          vuexContext.dispatch('logout');
+          return; 
+        }
+        vuexContext.commit('setToken', token);
+      },
+      logout(vuexContext) {
+        vuexContext.commit('clearToken');
+        Cookie.remove('jwt');
+        Cookie.remove('expirationDate');
+        if (process.client) {
+          localStorage.removeItem('token');
+          localStorage.removeItem('expirationDate');
+        }
       }
     }, 
     getters: {
       loadedPosts(state){
         return state.loadedPosts;
+      },
+      isAuth(state) {
+        return state.token != null;
       }
     }
   });
